5327ce9b9fa9f659d41761f422691a233709ccb4
Stan Aretinskiy <sr575sr@gmail.com>
2022-04-20 11:49:03 -0700

    Add ability to wait with custome timeout to methods that wait

--- uidemotest/src/utils/selenium_utils.py
+++ uidemotest/src/utils/selenium_utils.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
@@ -16,13 +16,16 @@ class SeleniumUtils:
         """
         return self.driver.find_element(locator[0], locator[1])
 
-    def wait_get_element(self, locator):
+    def wait_get_element(self, locator, timeout=None):
         """
         Wait for an element to be visible (standard timeout)
         :param locator: (locator_type, locator)
+        :param timeout: custom timeout in sec
         :return: WebElement
         """
-        return self.wait.until(EC.visibility_of_element_located(locator))
+        wait = WebDriverWait(self.driver, timeout) if timeout else self.wait
+
+        return wait.until(EC.visibility_of_element_located(locator))
 
     def get_text(self, mark):
         """
@@ -42,15 +45,16 @@ class SeleniumUtils:
         else:
             return element.get_attribute('value')
 
-    def wait_and_input_text(self, mark, text):
+    def wait_and_input_text(self, mark, text, timeout=None):
         """
         Wait for an element to be clickable (standard timeout), then click the element
         :param mark: WebElement or locator as (locator_type, locator)
         :param text: text as a string
+        :param timeout: custom timeout in sec
         :return: None
         """
         if not isinstance(mark, WebElement):
-            element = self.wait_get_element(mark)
+            element = self.wait_get_element(mark, timeout)
         else:
             element = mark
 
@@ -58,10 +62,13 @@ class SeleniumUtils:
         element.clear()
         element.send_keys(text)
 
-    def wait_and_click(self, mark):
+    def wait_and_click(self, mark, timeout=None):
         """
         Waiting for an element to be clickable using standard waiting time, then click the element
         :param mark: WebElement or locator as (locator_type, locator)
+        :param timeout: custom timeout in sec
         :return: None
         """
-        self.wait.until(EC.element_to_be_clickable(mark)).click()
+        wait = WebDriverWait(self.driver, timeout) if timeout else self.wait
+
+        wait.until(EC.element_to_be_clickable(mark)).click()
